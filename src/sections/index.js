ff9e99be5b6d5777beb92d953f374da4f8d638b8
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-08-26 05:04:11 +0100

    navbar

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+    Hero, PopularProducts, Services, SpecialOffer, SuperQuality, CustomerReviews, Subscribe, Footer
+}
